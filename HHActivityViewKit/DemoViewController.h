aa2739627388bd0473c6de528c673bbb6d3dedd2
ilingaohe <ilingaohe@gmail.com>
2013-12-19 15:15:33 +0800

    init status

---
+++ HHActivityViewKit/DemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoViewController.h
+//  HHActivityViewKit
+//
+//  Created by lingaohe on 12/19/13.
+//  Copyright (c) 2013 ilingaohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoViewController : UIViewController
+
+@end
